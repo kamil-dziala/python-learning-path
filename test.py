238bcd9047ea31806ece363e644c4e997b8107b4
kamil-dziala <kamil.dziala.pl@gmail.com>
2026-02-27 01:02:37 +0100

    Początek gita i githuba

---
+++ test.py
@@ -0,0 +1,9 @@
+imie = "Twój Nick"
+wiek = 25  # Liczb nie piszemy w cudzysłowie
+
+print("Cześć, nazywam się", imie)
+print("Mam", wiek, "lat.")
+
+# Możemy zmienić zawartość pudełka
+wiek = wiek + 1
+print("Za rok będę mieć", wiek, "lat!")
