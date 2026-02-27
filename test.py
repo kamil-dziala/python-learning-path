40862d72aea184d870cf270bffef81e5db1500f2
kamil-dziala <kamil.dziala.pl@gmail.com>
2026-02-27 01:06:43 +0100

    Początek gita i githuba

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-imie = "Twój Nick"
-wiek = 25  # Liczb nie piszemy w cudzysłowie
+wiek_str = input("Ile masz lat? ")
+# Musimy zamienić tekst na liczbę (integer), żeby móc ją porównać
+wiek = int(wiek_str)
 
-print("Cześć, nazywam się", imie)
-print("Mam", wiek, "lat.")
-
-# Możemy zmienić zawartość pudełka
-wiek = wiek + 1
-print("Za rok będę mieć", wiek, "lat!")
+if wiek >= 18:
+    print("Jesteś pełnoletni! Możesz wejść.")
+    print("Zapraszamy!")
+else:
+    print("Jesteś niepełnoletni.")
+    brakuje = 18 - wiek
+    print("Wróć do nas za " + str(brakuje) + " lat(a).")
